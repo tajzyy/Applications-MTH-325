8231118fd929021faff653ac4eca34a1e6c43ba4
tajzyy <travisjadam1@gmail.com>
2022-11-17 12:11:37 -0500

    is_symmetric added

--- Application_Three/main.py
+++ Application_Three/main.py
@@ -19,3 +19,29 @@ def is_reflexive(edge_list):
         return reflexive
 
     return reflexive
+
+
+def is_symmetric(edge_list):
+    symmetric = True
+    list_one, list_two = [], []
+    node_list1, node_list2 = [], []
+    count = 0
+
+    for edge in edge_list:
+        node1, node2 = edge
+        list_one.append(node1)
+        list_two.append(node2)
+
+    for i, j in zip(list_one, list_two):
+        if i != j:
+            node_list1.append(i)
+            node_list2.append(j)
+
+    node_list1.sort()
+    node_list2.sort()
+
+    if node_list1 != node_list2:
+        symmetric = False
+        return symmetric
+
+    return symmetric
