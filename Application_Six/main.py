123f26e00d173a8a97866c69b293f9ad5901a877
tajzyy <travisjadam1@gmail.com>
2022-12-01 14:01:30 -0500

    All working correctly

---
+++ Application_Six/main.py
@@ -0,0 +1,42 @@
+def maximal_elements(edge_list):
+    maximals = []
+    edge_list.sort()
+    count = 0
+
+    for edge in edge_list:
+        for i in edge_list:
+            if i % edge == 0:
+                count += 1
+        if count == 1:
+            maximals.append(edge)
+            count = 0
+        else:
+            count = 0
+
+    return maximals
+
+
+def greatest_element(edge_list):
+    greatest = []
+    edge_list.sort()
+    count = 0
+
+    for edge in edge_list:
+        for i in edge_list:
+            if i % edge == 0:
+                count += 1
+        if count == 1:
+            greatest.append(edge)
+            count = 0
+        else:
+            count = 0
+
+    if greatest.__len__() > 1:
+        greatest.clear()
+
+    return greatest
+
+
+print(maximal_elements([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 24]))
+print(greatest_element([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 24]))
+print(greatest_element([2, 4, 8, 16, 32]))
