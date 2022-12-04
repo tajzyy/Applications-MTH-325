f9f426f9879dcba817f265ac73d4be0e86f2073c
Travis Johnson <107153958+tajzyy@users.noreply.github.com>
2022-12-04 10:51:45 -0500

    Update max_or_great.py
    Revised

--- Application_Six/max_or_great.py
+++ Application_Six/max_or_great.py
@@ -8,20 +8,20 @@ def maximal_elements(edge_list):
         list_one.append(node1)
         list_two.append(node2)
 
-    for i in list_one:
-        if not list_one.count(i) > 1:
+    for i, j in zip(list_one, list_two):
+        if (not list_one.count(i) > 1) and (i == j):
             maximals.append(i)
-
-    maximals = [*set(maximals)]
 
     return maximals
 
 
 def greatest_element(edge_list):
     greatest = maximal_elements(edge_list)
-    
+
     if greatest.__len__() > 1:
         greatest.clear()
-        
+
     return greatest
 
+
+print(maximal_elements([(0, 1), (0, 2), (1, 2), (2, 3), (3, 1)]))
